152bbbc9a8680d18b38ca4fba2f9bdb09ee68862
Cloud Han <cloudhan@outlook.com>
2021-12-05 16:40:35 +0800

    add thrust cc_library

---
+++ examples/main_thrust.cc
@@ -0,0 +1,11 @@
+#include <thrust/device_vector.h>
+
+#include <iostream>
+
+int main() {
+  const int num_elements = 8192;
+  thrust::device_vector<float> vec(num_elements, 42.0);
+  auto sum =  thrust::reduce(vec.begin(), vec.end(), (float)0.0, thrust::plus<float>());
+  std::cout << "thrust device_vector created, sum reduce as " << sum << ", mean: " << sum / num_elements << std::endl;
+  return 0;
+}
